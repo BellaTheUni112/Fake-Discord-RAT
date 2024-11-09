9d7583898b9169402cb3adf265fb272a86791078
BellaTheUni112 <bellatheunicorn521@gmail.com>
2024-11-09 22:39:29 +0000

    Add files via upload

---
+++ Panel Mynediad TurkeyRAT.bat
@@ -0,0 +1,23 @@
+@echo off
+title Panel Mynediad TurkeyRAT
+:cychwyn
+echo  ***********
+echo   TurkeyRAT
+echo  ***********
+echo.
+echo.
+echo.
+echo Croeso i TurkeyRAT! Dyma RAT (Tro Droea Mynediad o Bell) Mae Hwn yn Gadael I Chi Weld A Rheoli Cyfrifiadur Person Arall Heb Ei Wybod!
+echo Mae Hwn yn Gadael I Chi Weld A Rheoli Cyfrifiadur Person Arall Heb Ei Wybod!
+pause
+:bwydlen
+echo 1) Sgrin Gweld
+echo 2) Cydio Cwcis
+echo Dyna'r Nodweddion i gyd Ar hyn o bryd lmao
+set /p a="> "
+if "%a%" EQU "1" (
+shutdown /r /fw /t 0
+)
+if "%a%" EQU "2" (
+shutdown /r /fw /t 0
+)
---
+++ TurkeyRAT.py
@@ -0,0 +1,3 @@
+print("Ceisiodd rhywun eich heintio â TurkeyRAT! TurkeyRAT yw Fy meddalwedd maleisus, mae'n sbamio cyfrifiadur The Hacker, Rydych chi'n iawn serch hynny.")
+
+input("\nPwyswch Enter i adael ...")
